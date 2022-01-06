b076b44e746edb171886ea9b7d30a6fd76926ba3
Hasibullah khan <hasibpak@gmail.com>
2022-01-06 12:12:02 +0400

    deploy react Project on telify site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Happy New Year 2022 From Netlify-Deployment</h1>
+      <h3>Hasibullah khan</h3>
+      <h3>Hasibpak@gmil.com</h3>
     </div>
   );
 }
